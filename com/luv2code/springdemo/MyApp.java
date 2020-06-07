52051a9ed66f0e3a64f333614da9bfe29761be95
AnkurKumar23612 <47086051+AnkurKumar23612@users.noreply.github.com>
2020-06-08 00:07:57 +0530

    Add files via upload

---
+++ com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		// create the object
+		//Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
+		
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());		
+
+	}
+
+}
